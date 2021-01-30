5ed391c636634097d6bc91785fa19c6fba7c765b
aayushinigam <aayushinigam2109@gmail.com>
2021-01-30 16:17:16 +0530

    phase1 complete

--- sort.py
+++ sort.py
@@ -10,6 +10,30 @@ FACTOR = 1048576  #value of 2^20 - to convert MB to bytes
 #here record implies tuples
 
 
+
+def buildMinHeap(array,n) :
+	for i in range(n/2-1,0) :
+		minHeapify(array,n,i)
+
+
+
+def minHeapify(array,n,i) :
+	largest = i
+	left = 2*i 
+	rigt = 2*i + 1
+
+	if(array[left] > array[largest]) :
+		largest = left
+	if(array[right] > array[largest]) :
+		largest = right
+	if(largest != i) :
+		array[largest],array[i] = array[i],array[largest]
+		minHeapify(array,n,largest) 
+	
+
+
+
+
 #getting metadata 
 def getMetaData() :
 	try :
@@ -29,15 +53,38 @@ def getMetaData() :
 		sys.exit(0)
 	return record_size
 
-#def compare()
+
+
+def phaseTwo(output_file,ram_size,num_of_subfiles,cols_to_sort_indices) :
+
+	subfiles = []
+	for i in range(0,num_of_subfiles) :
+		file_name = "subfile" + i + ".txt"
+		subfile = open(file_name,"r")
+		subfiles.append(subfile)
+
+
+	#create the outfile file
+	with open(output_file,'w') as outfile :
+		for i in range(0,num_of_subfiles) :
+			file_name = "subfile" + i + ".txt"
+			with open(file_name,'r') as subfile :
+				data = subfile.readlines()
 
 
 
-def phaseOne(main_memory_size,input_file,output_file,cols_to_sort,sorting_order):
+
+
+
+
+
+
+
+def phaseOne(ram_size,input_file,output_file,cols_to_sort,sorting_order):
 
 	#get meta data 
 	record_size = getMetaData()
-	print("##running phase 1")
+	print("###running phase 1")
 	total_records = 0
 	cols_to_sort_indices = []
 	#read input file to count no of records in the file
@@ -54,35 +101,39 @@ def phaseOne(main_memory_size,input_file,output_file,cols_to_sort,sorting_order)
 	#get the column indices which need to be sorted :
 	for i in cols_to_sort: 
 		cols_to_sort_indices.append(metadata[i][1])
-
-
+	
 	#calculate number of records in each file 
-	main_memory_size = main_memory_size * FACTOR 
-	subfile_record = main_memory_size//(record_size)
+	ram_size = ram_size * FACTOR 
+	subfile_record = ram_size//(record_size)
 
 	#calculate no of subfiles 
-	subfiles = math.ceil(total_records/subfile_record)
-	print("Number of subfiles :" , subfiles)
+	num_of_subfiles = math.ceil(total_records/subfile_record)
+	print("Number of subfiles :" , num_of_subfiles)
 
-	#print("no of record in each file :",subfile_record)
+
+	###SPLITTING THE MAIN FILE AND SORTING EACH SUBFILE
 	with open(input_file) as inp :
 		data = inp.readlines()
-		sublist = []
-		temp = 0
-		subfile = 1
-		temp = 0
-		for i in range(0,subfiles) :
-			for j in range(temp,temp+subfile_record) :
+		temp = 0	   
+		subfile = 1    #stores the current subfile no. that is being sorted
+		for i in range(0,num_of_subfiles) :
+			sublist = []   #stores the records for each file before getting written into subfile
+			#splitting the main file
+			for j in range(temp, temp + subfile_record) :
+				#print(j)
+				if(j == len(data)) :
+					break
 				data_cols = data[j].strip().split('  ')
 				sublist.append(data_cols)
-			temp += 1
-			print(f"###sorting sublist{i}")
+			temp += subfile_record
+
+			#sorting the data
+			print(f"###sorting sublist{i+1}")
 			if(sorting_order == 'asc') :
 				sublist = sorted(sublist,key=itemgetter(*cols_to_sort_indices))
 			else :
 				sublist = sorted(sublist,key=itemgetter(*cols_to_sort_indices), reverse=True)
 			file_name = 'subfile' + str(subfile) + '.txt'
-
 
 			#writing the sublist to subfile
 			with open(file_name,'w') as f:
@@ -91,6 +142,14 @@ def phaseOne(main_memory_size,input_file,output_file,cols_to_sort,sorting_order)
 						f.write(str(j))
 						f.write('  ')
 					f.write('\n')
+			subfile += 1
+
+
+	#MERGE THESE SUBFILES INTO ONE FILE :
+	phaseTwo(output_file,ram_size,num_of_subfiles,cols_to_sort_indices)
+
+
+
 
 
 #program starts here 
@@ -101,7 +160,7 @@ if __name__ == "__main__" :
 	#store the input - 
 	input_file = sys.argv[1]
 	output_file = sys.argv[2]
-	main_memory_size = int(sys.argv[3])
+	ram_size = int(sys.argv[3])
 
 	#WITHOUT THREADING
 	if(sys.argv[4] == 'asc'  or sys.argv[3] == 'desc') :
@@ -118,8 +177,8 @@ if __name__ == "__main__" :
 	for i in range(temp,len(sys.argv)) :
 		cols_to_sort.append(sys.argv[i])
 
-	
-	phaseOne(main_memory_size,input_file,output_file,cols_to_sort,order)
+	#Execute phase 1 : 
+	phaseOne(ram_size,input_file,output_file,cols_to_sort,order)
 
 
 
